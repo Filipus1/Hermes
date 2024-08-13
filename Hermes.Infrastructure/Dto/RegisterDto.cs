0f2b62ee4f3ca2eb431d47caceb0418206c92976
Filipus <filip.zytka@gmail.com>
2024-08-13 22:19:26 +0200

    whitespace

--- Hermes.Infrastructure/Dto/RegisterDto.cs
+++ Hermes.Infrastructure/Dto/RegisterDto.cs
@@ -7,10 +7,11 @@ public class RegisterDto
     [EmailAddress(ErrorMessage = "Invalid email address.")]
     public string Email { get; set; } = string.Empty;
 
+    [Required]
     [StringLength(30, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 30 characters.")]
     [RegularExpression(@"(?=.*\d)(?=.*[A-Z]).{8,}", ErrorMessage = "Password must be at least 8 characters long, contain at least one number and one uppercase letter in Dto.")]
-    [Required]
     public string Password { get; set; } = string.Empty;
+
     [Required]
     public string Token { get; set; } = string.Empty;
 }
