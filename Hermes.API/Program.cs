d37f70706f29e2747d5a61fbb07067ab01f4f260
Filipus <filip.zytka@gmail.com>
2024-08-13 20:47:10 +0200

    delete double middlewares??

--- Hermes.API/Program.cs
+++ Hermes.API/Program.cs
@@ -98,10 +98,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors("cors-policy");
-app.UseAuthentication();
-app.UseAuthorization();
-
 app.MapControllers();
 
 await app.Services.SeedAdminUser();
