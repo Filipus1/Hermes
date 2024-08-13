1043e14b2001e7f22b946bf1ab61c1f69f007126
Filipus <filip.zytka@gmail.com>
2024-08-13 19:57:51 +0200

    max password length is 30

--- Hermes.Application/Entities/User.cs
+++ Hermes.Application/Entities/User.cs
@@ -12,6 +12,7 @@ public class User
     public string Email { get; set; } = string.Empty;
 
     [Required]
+    [StringLength(30, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 30 characters.")]
     [RegularExpression(@"(?=.*\d)(?=.*[A-Z]).{8,}",
     ErrorMessage = "Password must be at least 8 characters long, contain at least one number and one uppercase letter.")]
     public string Password { get; set; } = string.Empty;
