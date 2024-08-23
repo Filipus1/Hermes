995ffe8b4d324e1fd9b8a511d11154be8f3248b3
Filipus <filip.zytka@gmail.com>
2024-08-23 16:18:54 +0200

    controllers namespace

--- Hermes.API/Controller/BanListController.cs
+++ Hermes.API/Controller/BanListController.cs
@@ -1,7 +1,7 @@
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Hermes.API;
+namespace Hermes.API.Controllers;
 [ApiController]
 [Route("api/ban")]
 public class BanListController : Controller
