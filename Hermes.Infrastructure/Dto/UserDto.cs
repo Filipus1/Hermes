774ec39400079068b8ed769410916e94bd52c476
Filipus <filip.zytka@gmail.com>
2024-09-26 18:14:49 +0200

    100 to 30 max characters in userDto

--- Hermes.Infrastructure/Dto/UserDto.cs
+++ Hermes.Infrastructure/Dto/UserDto.cs
@@ -8,7 +8,7 @@ public record UserDto
     public string Email { get; set; } = string.Empty;
 
     [Required]
-    [StringLength(30, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters.")]
+    [StringLength(30, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 30 characters.")]
     [RegularExpression(@"(?=.*\d)(?=.*[A-Z]).{8,}", ErrorMessage = "Password must be at least 8 characters long, contain at least one number and one uppercase letter in Dto.")]
     public string Password { get; set; } = string.Empty;
 }
